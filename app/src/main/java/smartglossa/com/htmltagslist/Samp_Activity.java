20cbad09f0f2e810793521f40b43d029d20172b3
sathishSmart <sathishprs@smartglossa.com>
2017-05-22 16:39:27 +0530

    Add class file

---
+++ app/src/main/java/smartglossa/com/htmltagslist/Samp_Activity.java
@@ -0,0 +1,18 @@
+package smartglossa.com.htmltagslist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by smartglossa2 on 22/5/17.
+ */
+
+public class Samp_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.samp_main);
+    }
+
+}
