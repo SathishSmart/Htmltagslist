642f35a80e1204138bc6cec2b222f85e68aa2f1c
sathishSmart <sathishprs@smartglossa.com>
2017-06-03 13:02:22 +0530

    change xml file

--- app/src/main/java/smartglossa/com/htmltagslist/MainActivity.java
+++ app/src/main/java/smartglossa/com/htmltagslist/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button html=(Button)findViewById(R.id.htmlbutt);
+        Button html=(Button) findViewById(R.id.htmlbutt);
         Button base=(Button)findViewById(R.id.basebutt);
         Button head=(Button)findViewById(R.id.headbutt);
         Button link=(Button)findViewById(R.id.likebutt);
